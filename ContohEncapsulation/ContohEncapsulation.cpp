4781c6fc204a4132ff5eba8c6d3a22d71446e64c
Yusuf Hidayat <sufhidayat9@gmail.com>
2024-05-27 10:18:16 +0700

    int main

--- ContohEncapsulation/ContohEncapsulation.cpp
+++ ContohEncapsulation/ContohEncapsulation.cpp
@@ -12,3 +12,18 @@ public:
 		return saklarNo[i];
 	}
 };
+
+int main() {
+	remoteLampu lampuRumah;
+
+	lampuRumah.setSaklarNo(1, "Lampu Teras Rumah");
+	lampuRumah.setSaklarNo(2, "Lampu Ruang Tamu");
+	lampuRumah.setSaklarNo(3, "Lampu Kamar Tidur");
+	lampuRumah.setSaklarNo(4, "Lampu Dapur");
+
+	cout << lampuRumah.getSaklarNo(1) << endl;
+	cout << lampuRumah.getSaklarNo(2) << endl;
+	cout << lampuRumah.getSaklarNo(3) << endl;
+	cout << lampuRumah.getSaklarNo(4) << endl;
+	return 0;
+}
